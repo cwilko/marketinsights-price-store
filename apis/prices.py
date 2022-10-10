32543f8b8d0bbca999881306458743c48573a6a2
cwilko <chris.wilkinson@uk.ibm.com>
2022-10-10 23:52:59 +0100

    MDS support for multi-source tables

--- apis/prices.py
+++ apis/prices.py
@@ -23,25 +23,24 @@ class Keys(Resource):
         return jsonify(results)
 
 
-@api.route('/aggregate')
+@api.route('/aggregate/<table_id>')
 class AggregatePrices(Resource):
 
     parser = reqparse.RequestParser()
     parser.add_argument('start', help='Start timestamp (optional)')
     parser.add_argument('end', help='End timestamp (optional)')
-    parser.add_argument('unit', required=True, help='Sample unit for requested data, e.g: 1H, 5min')
     parser.add_argument('sources', action='append', required=True, help='Precedence ordered list of datasources to read from')
 
     @api.expect(parser, validate=True)
     @api.doc(description='Get prices based on an aggregation of datasources')
-    def get(self):
+    def get(self, table_id):
 
         args = self.parser.parse_args()
         start = args["start"] if args["start"] else "1979-01-01"
         end = args["end"] if args["end"] else "2050-01-01"
 
         try:
-            results = mds.aggregate(args["sources"], args["unit"], start, end, True)
+            results = mds.aggregate(table_id, args["sources"], start, end, debug=True)
             if results is not None:
                 results = results.reset_index().to_json(orient='split', date_format="iso")
             results = {"rc": "success", "body": results}
@@ -50,17 +49,14 @@ class AggregatePrices(Resource):
         return jsonify(results)
 
 
-@api.route('/datasource/<source_id>')
+@api.route('/datasource/<table_id>')
 class Prices(Resource):
 
-    parser = reqparse.RequestParser()
-    parser.add_argument('unit', required=True, help='Sample unit for datasource, e.g: 1H, 5min')
-
     @api.doc(description='Get price data from a datasource')
-    def get(self, source_id):
+    def get(self, table_id):
 
         try:
-            results = mds.get(source_id)
+            results = mds.get(table_id)
             if results is not None:
                 results = results.reset_index().to_json(orient='split', date_format="iso")
             results = {"rc": "success", "body": results}
@@ -68,34 +64,30 @@ class Prices(Resource):
             results = {"rc": "fail", "msg": str(e)}
         return jsonify(results)
 
-    @api.expect(parser, validate=True)
     @api.doc(description='Add new price data to a datasource')
-    def post(self, source_id):
+    def post(self, table_id):
 
-        args = self.parser.parse_args()
         data = pandas.read_json(json.dumps(request.get_json()), orient='split').set_index(["Date_Time", "ID"])
         # Uncomment the following on pandas < 1.2.0
         # data.index = data.index.tz_localize('UTC')
 
         try:
-            mds.append(source_id, data, args["unit"])
+            mds.append(table_id, data)
             results = {"rc": "success"}
         except ValueError as e:
             results = {"rc": "fail", "msg": str(e)}
 
         return jsonify(results)
 
-    @api.expect(parser, validate=True)
     @api.doc(description='Add or update existing price data for a datasource')
-    def put(self, source_id):
+    def put(self, table_id):
 
-        args = self.parser.parse_args()
         data = pandas.read_json(json.dumps(request.get_json()), orient='split').set_index(["Date_Time", "ID"])
         # Uncomment the following on pandas < 1.2.0
         # data.index = data.index.tz_localize('UTC')
 
         try:
-            mds.append(source_id, data, args["unit"], update=True)
+            mds.append(table_id, data, update=True)
             results = {"rc": "success"}
         except ValueError as e:
             results = {"rc": "fail", "msg": str(e)}
@@ -103,10 +95,10 @@ class Prices(Resource):
         return jsonify(results)
 
     @api.doc(description='Remove a datasource and all its existing price data')
-    def delete(self, source_id):
+    def delete(self, table_id):
 
         try:
-            mds.delete(source_id)
+            mds.delete(table_id)
             results = {"rc": "success"}
         except Exception as e:
             results = {"rc": "fail", "msg": str(e)}
